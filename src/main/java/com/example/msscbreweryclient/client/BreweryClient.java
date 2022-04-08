797558938ce13ac53d7a6bdfa4bed47a74f7d82e
mauriziosodano <maurizio.sodano@gmail.com>
2022-04-08 16:43:55 +0200

    created Client with RestTemplate

---
+++ src/main/java/com/example/msscbreweryclient/client/BreweryClient.java
@@ -0,0 +1,29 @@
+package com.example.msscbreweryclient.client;
+
+import com.example.msscbreweryclient.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+    private final String BEER_PATH_V1="/api/v1/beer/";
+    private final RestTemplate restTemplate;
+
+    private String apihost;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID beerId) {
+        return restTemplate.getForObject(apihost+BEER_PATH_V1+beerId.toString(),BeerDto.class);
+    }
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
